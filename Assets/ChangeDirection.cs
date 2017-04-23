b56887e8fea01807d9646f6c334faf0ce36c6d93
ZacLindsey <zaclindsey1996@pointloma.edu>
2017-04-23 15:39:19 -0700

    no message

--- Assets/ChangeDirection.cs
+++ Assets/ChangeDirection.cs
@@ -7,6 +7,9 @@ public class ChangeDirection : MonoBehaviour
     public Vector3 newDirection1 = new Vector3();
     public Vector3 newDirection2 = new Vector3();
 
+    public Vector3 newRotation1 = new Vector3();
+    public Vector3 newRotation2 = new Vector3();
+
     // Use this for initialization
     void Start()
     {
@@ -25,6 +28,7 @@ public class ChangeDirection : MonoBehaviour
         int dir = Random.Range(0, 2);
 
         eu.GetComponent<Rigidbody>().velocity = Vector3.zero;
+        eu.GetComponent<Rigidbody>().rotation = Quaternion.identity;
 
         if (eu != null)
         {
@@ -33,12 +37,20 @@ public class ChangeDirection : MonoBehaviour
                 eu.velocity.x = newDirection1.x * 100.0f;
                 eu.velocity.y = newDirection1.y * 100.0f;
                 eu.velocity.z = newDirection1.z * 100.0f;
+
+                eu.rotation.x = newRotation1.x;
+                eu.rotation.y = newRotation1.y;
+                eu.rotation.z = newRotation1.z;
             }
             else if (dir == 1)
             {
                 eu.velocity.x = newDirection2.x * 100.0f;
                 eu.velocity.y = newDirection2.y * 100.0f;
                 eu.velocity.z = newDirection2.z * 100.0f;
+
+                eu.rotation.x = newRotation2.x;
+                eu.rotation.y = newRotation2.y;
+                eu.rotation.z = newRotation2.z;
             }
         }
     }
--- Assets/EnemyUnit.cs
+++ Assets/EnemyUnit.cs
@@ -6,17 +6,20 @@ public class EnemyUnit : MonoBehaviour
 
     public float health;
     public Vector3 velocity = new Vector3();
+    public Vector3 rotation = new Vector3();
 
     // Use this for initialization
     void Start()
     {
         GetComponent<Rigidbody>().velocity = velocity;
+        GetComponent<Rigidbody>().rotation = Quaternion.identity;
     }
 
     // Update is called once per frame
     void Update()
     {
         GetComponent<Rigidbody>().velocity = velocity;
+        GetComponent<Rigidbody>().rotation = Quaternion.Euler(rotation);
     }
 
     public void explode()
